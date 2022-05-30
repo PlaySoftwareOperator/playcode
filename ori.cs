de2a0251e5fc3728685a2218e2d1f6a5c51b318a
PlaySoftwareOperator <96362581+PlaySoftwareOperator@users.noreply.github.com>
2022-05-30 22:34:54 +0900

    Create ori.cs

---
+++ ori.cs
@@ -0,0 +1,33 @@
+using system;
+
+
+namespace abc{
+  public class test{
+
+    public int a;
+  
+  
+    public test(){
+  
+      Console.WriteLine("oh!!!!!");
+  
+    }
+  
+    public static void Main(){
+      
+  
+      
+  
+  
+  
+    }
+
+
+
+  }
+
+
+
+
+
+}
